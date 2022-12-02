27cbfa711542721527361acb3c92ba50fa2edea0
Kanji Tanaka <ktanaka@ichain.co.jp>
2022-12-02 23:09:01 +0900

    Setup plugin development

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
-export function sum (...a: number[]): number {
-  return a.reduce((acc, val) => acc + val, 0)
+import * as Serverless from 'serverless'
+import * as Plugin from 'serverless/classes/Plugin'
+
+export default class ServerlessAssumeRole {
+  serverless: Serverless
+  options: Serverless.Options
+  log: Plugin.Logging['log']
+  hooks: Plugin.Hooks
+  commands: Plugin.Commands
+
+  constructor (
+    serverless: Serverless,
+    options: Serverless.Options,
+    utils: Plugin.Logging
+  ) {
+    this.serverless = serverless
+    this.options = options
+    this.log = utils.log
+
+    this.commands = {
+      'assumerole:test': {
+        usage: 'My custom command',
+        lifecycleEvents: ['run']
+      }
+    }
+
+    this.hooks = {
+      'assumerole:test:run': this.run.bind(this)
+    }
+  }
+
+  run (): void {
+    this.log.error('error')
+    this.log.warning('warning')
+    this.log.notice('notice')
+    this.log.info('info')
+    this.log.debug('debug')
+  }
 }
+
+module.exports = ServerlessAssumeRole
