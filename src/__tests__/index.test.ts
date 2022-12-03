8f8b68392e65c734f9e307ee2636a66fbbd1f554
Kanji Tanaka <ktanaka@ichain.co.jp>
2022-12-03 11:29:21 +0900

    Add validation

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,220 @@
-import { sum } from '..'
+import * as Serverless from 'serverless'
+import ServerlessAssumeRole from '..'
+import { AssumeRoleInputs, ServerlessV3, ServerlessUtils } from '../types'
 
-it('passes', () => {
-  expect(sum(1, 2, 3, 4)).toEqual(10)
+describe('ServerlessAssumeRole', () => {
+  let utils: ServerlessUtils
+
+  function mockServerless (config: AssumeRoleInputs): ServerlessV3 {
+    const sls: any = {
+      getProvider: jest.fn(() => ({
+        getCredentials: jest.fn(() => ({}))
+      })),
+      service: {
+        custom: {
+          assumeRole: config
+        }
+      },
+      classes: { Error }
+    }
+    return sls
+  }
+
+  beforeEach(() => {
+    utils = {
+      log: {
+        error: jest.fn(),
+        warning: jest.fn(),
+        notice: jest.fn(),
+        info: jest.fn(),
+        debug: jest.fn(),
+        success: jest.fn(),
+        verbose: jest.fn()
+      }
+    }
+  })
+
+  describe('constructor', () => {
+    function subject (config: any): ServerlessAssumeRole {
+      const serverless = mockServerless(config)
+      const options: Serverless.Options = { stage: 'dev', region: 'local' }
+      return new ServerlessAssumeRole(serverless, options, utils)
+    }
+
+    it('sets "DurationSeconds" to assume role inputs from "custom" configuration', () => {
+      const pluginStr = subject({ durationSeconds: '123' })
+      expect(pluginStr.input.DurationSeconds).toEqual(123)
+
+      const pluginNum = subject({ durationSeconds: 456 })
+      expect(pluginNum.input.DurationSeconds).toEqual(456)
+    })
+
+    it('sets undefined as "DurationSeconds" to assume role inputs if "custom" configuration isn\'t set', () => {
+      const plugin = subject({})
+      expect(plugin.input.DurationSeconds).toBeUndefined()
+    })
+
+    it('sets "ExternalId" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ externalId: 'foobar' })
+      expect(plugin.input.ExternalId).toEqual('foobar')
+    })
+
+    it('sets "Policy" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ policy: 'foobar' })
+      expect(plugin.input.Policy).toEqual('foobar')
+    })
+
+    it('sets "PolicyArns" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ policyArns: [{ arn: 'foobar' }] })
+      expect(plugin.input.PolicyArns).toEqual([{ arn: 'foobar' }])
+    })
+
+    it('sets an empty array as "PolicyArns" to assume role inputs if "custom" configuration is not set', () => {
+      const plugin = subject({})
+      expect(plugin.input.PolicyArns).toEqual([])
+    })
+
+    it('sets "RoleArn" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ roleArn: 'foobar' })
+      expect(plugin.input.RoleArn).toEqual('foobar')
+    })
+
+    it('sets "RoleSessionName" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ roleSessionName: 'foobar' })
+      expect(plugin.input.RoleSessionName).toEqual('foobar')
+    })
+
+    it('sets "SerialNumber" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ serialNumber: 'foobar' })
+      expect(plugin.input.SerialNumber).toEqual('foobar')
+    })
+
+    it('sets "SourceIdentity" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ sourceIdentity: 'foobar' })
+      expect(plugin.input.SourceIdentity).toEqual('foobar')
+    })
+
+    it('sets "Tags" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ tags: [{ key: 'foo', value: 'bar' }] })
+      expect(plugin.input.Tags).toEqual([{ Key: 'foo', Value: 'bar' }])
+    })
+
+    it('sets an empty array as "Tags" to assume role inputs if "custom" configuration is not set', () => {
+      const plugin = subject({})
+      expect(plugin.input.Tags).toEqual([])
+    })
+
+    it('sets "TokenCode" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ tokenCode: 'foobar' })
+      expect(plugin.input.TokenCode).toEqual('foobar')
+    })
+
+    it('sets "TransitiveTagKeys" to assume role inputs from "custom" configuration', () => {
+      const plugin = subject({ transitiveTagKeys: ['foo', 'bar'] })
+      expect(plugin.input.TransitiveTagKeys).toEqual(['foo', 'bar'])
+    })
+
+    it('sets an empty array as "TransitiveTagKeys" to assume role inputs if "custom" configuration is not set', () => {
+      const plugin = subject({})
+      expect(plugin.input.TransitiveTagKeys).toEqual([])
+    })
+
+    it('throws an error if "durationSeconds" is neither undefined nor an interger', () => {
+      const error = /durationSeconds should be an integer/
+
+      expect(subject({ durationSeconds: undefined }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+      expect(subject({ durationSeconds: 123 }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+
+      expect(() => subject({ durationSeconds: 'foo' })).toThrow(error)
+      expect(() => subject({ durationSeconds: ['foo', 'bar'] })).toThrow(error)
+      expect(() => subject({ durationSeconds: {} })).toThrow(error)
+    })
+
+    const stringFields = [
+      'externalId',
+      'policy',
+      'roleArn',
+      'roleSessionName',
+      'serialNumber',
+      'sourceIdentity',
+      'tokenCode'
+    ]
+
+    it.each(stringFields)('throws an error if "%s" is neither undefined nor a string', (field) => {
+      const error = new RegExp(`${field} should be a string`)
+
+      expect(subject({ [field]: undefined }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+      expect(subject({ [field]: 'foo' }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+
+      expect(() => subject({ [field]: 123 })).toThrow(error)
+      expect(() => subject({ [field]: ['foo', 'bar'] })).toThrow(error)
+      expect(() => subject({ [field]: {} })).toThrow(error)
+    })
+
+    it('throws an error if "policyArns" is neither undefined nor an array of object having arn field', () => {
+      /*
+       * PolicyDectatorType
+       * See: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-sts/interfaces/policydescriptortype.html
+       */
+      const error = /policyArns should be an array of object having "arn" field/
+
+      expect(subject({ policyArns: undefined }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+      expect(subject({ policyArns: [{ arn: 'bar' }, { arn: 'foobar' }] }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+
+      expect(() => subject({ policyArns: 123 })).toThrow(error)
+      expect(() => subject({ policyArns: 'foobar' })).toThrow(error)
+      expect(() => subject({ policyArns: ['foo', 'bar'] })).toThrow(error)
+      expect(() => subject({ policyArns: [{ arn: 123 }] })).toThrow(error)
+      expect(() => subject({ policyArns: [{}, { arn: 'foo' }] })).toThrow(error)
+      expect(() => subject({ policyArns: {} })).toThrow(error)
+    })
+
+    it('throws an error if "tags" is neither undefined nor an array of object having key and value field', () => {
+      /*
+       * PolicyDectatorType
+       * See: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-sts/interfaces/policydescriptortype.html
+       */
+      const error = /tags should be an array of object having "key" and "value" field/
+
+      expect(subject({ tags: undefined }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+      expect(subject({
+        tags: [{ key: 'bar', value: 'foo' }, { key: 'foo', value: 'bar' }]
+      })).toBeInstanceOf(ServerlessAssumeRole)
+
+      expect(() => subject({ tags: 123 })).toThrow(error)
+      expect(() => subject({ tags: 'foobar' })).toThrow(error)
+      expect(() => subject({ tags: ['foo', 'bar'] })).toThrow(error)
+      expect(() => subject({ tags: [{ key: 123, value: 'foo' }] })).toThrow(error)
+      expect(() => subject({
+        tags: [{ key: 'foo' }, { key: 'foo', value: 'bar' }]
+      })).toThrow(error)
+      expect(() => subject({ tags: {} })).toThrow(error)
+    })
+
+    it('throws an error if "transitiveTagKeys" is neither undefined nor an array of string', () => {
+      /*
+       * PolicyDectatorType
+       * See: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-sts/interfaces/policydescriptortype.html
+       */
+      const error = /transitiveTagKeys should be an array of string/
+
+      expect(subject({ transitiveTagKeys: undefined }))
+        .toBeInstanceOf(ServerlessAssumeRole)
+      expect(subject({
+        transitiveTagKeys: ['foo', 'bar']
+      })).toBeInstanceOf(ServerlessAssumeRole)
+
+      expect(() => subject({ transitiveTagKeys: 123 })).toThrow(error)
+      expect(() => subject({ transitiveTagKeys: 'foobar' })).toThrow(error)
+      expect(() => subject({ transitiveTagKeys: [123, 'foo'] })).toThrow(error)
+      expect(() => subject({ transitiveTagKeys: {} })).toThrow(error)
+    })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import * as Serverless from 'serverless'
 import * as Plugin from 'serverless/classes/Plugin'
+import { AssumeRoleCommandInput, PolicyDescriptorType, Tag } from '@aws-sdk/client-sts'
+import { ServerlessV3, ServerlessUtils } from './types'
 
 export default class ServerlessAssumeRole {
-  serverless: Serverless
+  serverless: ServerlessV3
   options: Serverless.Options
-  log: Plugin.Logging['log']
+  log: ServerlessUtils['log']
+  input: AssumeRoleCommandInput
   hooks: Plugin.Hooks
   commands: Plugin.Commands
 
   constructor (
-    serverless: Serverless,
+    serverless: ServerlessV3,
     options: Serverless.Options,
-    utils: Plugin.Logging
+    utils: ServerlessUtils
   ) {
     this.serverless = serverless
     this.options = options
     this.log = utils.log
+    this.input = this.getAssumeRoleInput()
 
     this.commands = {
       'assumerole:test': {
@@ -25,17 +29,128 @@ export default class ServerlessAssumeRole {
     }
 
     this.hooks = {
-      'assumerole:test:run': this.run.bind(this)
+      'assumerole:test:run': this.run.bind(this),
+      'before:deploy:deploy': this.run.bind(this)
+    }
+  }
+
+  private getAssumeRoleInput (): AssumeRoleCommandInput {
+    const {
+      durationSeconds,
+      externalId,
+      policy,
+      policyArns,
+      roleArn,
+      roleSessionName,
+      serialNumber,
+      sourceIdentity,
+      tags,
+      tokenCode,
+      transitiveTagKeys
+    } = this.serverless.service.custom?.assumeRole ?? {}
+
+    const intDurationSeconds = Number.parseInt(durationSeconds, 10)
+    if (durationSeconds !== undefined && Number.isNaN(intDurationSeconds)) {
+      throw this.error('durationSeconds should be an integer')
+    }
+
+    if (externalId !== undefined && typeof externalId !== 'string') {
+      throw this.error('externalId should be a string')
+    }
+
+    if (policy !== undefined && typeof policy !== 'string') {
+      throw this.error('policy should be a string')
+    }
+
+    if (
+      policyArns !== undefined &&
+      !(Array.isArray(policyArns) && policyArns.every(isPolicyDescriptorType))
+    ) {
+      throw this.error('policyArns should be an array of object having "arn" field')
+    }
+
+    if (roleArn !== undefined && typeof roleArn !== 'string') {
+      throw this.error('roleArn should be a string')
+    }
+
+    if (roleSessionName !== undefined && typeof roleSessionName !== 'string') {
+      throw this.error('roleSessionName should be a string')
+    }
+
+    if (serialNumber !== undefined && typeof serialNumber !== 'string') {
+      throw this.error('serialNumber should be a string')
+    }
+
+    if (sourceIdentity !== undefined && typeof sourceIdentity !== 'string') {
+      throw this.error('sourceIdentity should be a string')
+    }
+
+    if (
+      tags !== undefined &&
+      !(Array.isArray(tags) && tags.every(isValidTag))
+    ) {
+      throw this.error('tags should be an array of object having "key" and "value" field')
+    }
+
+    if (tokenCode !== undefined && typeof tokenCode !== 'string') {
+      throw this.error('tokenCode should be a string')
+    }
+
+    if (
+      transitiveTagKeys !== undefined &&
+      !(
+        Array.isArray(transitiveTagKeys) &&
+        transitiveTagKeys.every((val) => typeof val === 'string')
+      )
+    ) {
+      throw this.error('transitiveTagKeys should be an array of string')
+    }
+
+    return {
+      DurationSeconds: durationSeconds === undefined
+        ? undefined
+        : intDurationSeconds,
+      ExternalId: externalId,
+      Policy: policy,
+      PolicyArns: policyArns ?? [],
+      RoleArn: roleArn,
+      RoleSessionName: roleSessionName,
+      SerialNumber: serialNumber,
+      SourceIdentity: sourceIdentity,
+      Tags: toTags(tags),
+      TokenCode: tokenCode,
+      TransitiveTagKeys: transitiveTagKeys ?? []
     }
   }
 
   run (): void {
-    this.log.error('error')
-    this.log.warning('warning')
-    this.log.notice('notice')
-    this.log.info('info')
-    this.log.debug('debug')
+    this.log.notice('test')
   }
+
+  error (message?: string): Error {
+    return new this.serverless.classes.Error(message)
+  }
+}
+
+function isPolicyDescriptorType (obj: any): obj is PolicyDescriptorType {
+  if (typeof obj !== 'object') {
+    return false
+  }
+  return typeof obj.arn === 'string'
+}
+
+function isValidTag (obj: any): boolean {
+  if (typeof obj !== 'object') {
+    return false
+  }
+  return typeof obj.key === 'string' && typeof obj.value === 'string'
+}
+
+function toTags (tags: any[] | undefined): Tag[] {
+  if (tags === undefined) {
+    return []
+  }
+  return tags.map(({ key, value }) => ({ Key: key, Value: value }))
 }
 
 module.exports = ServerlessAssumeRole
